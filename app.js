f3ca5fc1014019d509bf4a78314b0f9c53481083
La Fourme <gaetanfourment@hotmail>
2026-09-13 22:55:29 +0200

    Uniformiser l espacement de l historique global

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const supabaseUrl = "https://fbvsgvdrdblxvmzutpjk.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZidnNndmRyZGJseHZtenV0cGprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4OTMzMDcsImV4cCI6MjEwNDQ2OTMwN30.iuISscmFcGTGCDiFOA0XVkGCgTaSFo-vkVh9_t5odi0";
 const supabaseClient = createSupabaseClient();
-const appBuildVersion = "20260913-4";
+const appBuildVersion = "20260913-5";
 const appBuildVersionStorageKey = "cles-app-build-version-v1";
 const appBuildReloadStorageKey = "cles-app-build-reload-v1";
 const appBuildVersionUrl = "app-version.json";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const runtimeCacheName = "cles-runtime-20260913-4";
+const runtimeCacheName = "cles-runtime-20260913-5";
 
 self.addEventListener("install", () => {
   self.skipWaiting();
